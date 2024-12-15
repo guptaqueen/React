dc35ab83ed1da611e73a381b64bb50e321b69d17
Manisha Gupta <rouniyarmanisha@gmail.com>
2024-12-15 21:32:54 +0545

    2nd

---
+++ src/Hello.jsx
@@ -0,0 +1,7 @@
+function Hello(){
+  let firstName = 'Manisha';
+  let lastName = 'Gupta'
+  let rollNo = '5000'
+  return <h3>My name is Manisha Gupta  {firstName}  {lastName} {rollNo}</h3>
+}
+export default Hello;
