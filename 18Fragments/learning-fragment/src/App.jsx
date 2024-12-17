d509a5e77119c6a71b94665fd5e9615a56da6180
Manisha Gupta <rouniyarmanisha@gmail.com>
2024-12-17 12:45:23 +0545

    3rd

---
+++ 18Fragments/learning-fragment/src/App.jsx
@@ -0,0 +1,23 @@
+
+// import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    // <React.Fragment>
+    <>
+      <h1>Healthy Food</h1>;
+      <ul class="list-group">
+        <li class="list-group-item">Rice</li>
+        <li class="list-group-item">Dal</li>
+        <li class="list-group-item">Vegetable</li>
+        <li class="list-group-item">Roti</li>
+        <li class="list-group-item">Milk</li>
+      </ul>
+      </>
+    // </React.Fragment>
+  );
+}
+
+export default App;
